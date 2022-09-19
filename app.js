511a0d9fdb8ed33ba930c486e8d55b642c2c5909
kane1997 <648636410@qq.com>
2022-09-20 09:32:26 +1000

    slightly changed

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello Konecer!')
+// app.get('/', (req, res) => {
+//   res.send('Hello Konecer!')
+// })
+
+
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
 })
 
-
-// app.use((req,res,next)=>{
-//     console.log('In the middleware');
-//     next();
-// })
-
-// app.use((req,res,next)=>{
-//     console.log('In another middleware');
-// })
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
