0780406eeb54a244b295c1a592cba4eb309617c5
kane1997 <648636410@qq.com>
2022-09-20 10:28:46 +1000

    add in officepc

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const bodyParser = require('body-parser');
+const port = 3000
+const indexRoutes = require('./routes/index');
+const usersRoutes = require('./routes/users');
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Konecer!')
+
+app.get('/', (req, res) => {
+  res.send('Hello Konecer!')
+})
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(indexRoutes);
+
+app.use(usersRoutes);
+// app.use('/addorder',(req,res,next)=>{
+//   console.log('In another middleware');
+//   res.send("<h1>The Add Order Page</h1>");
+//   next();
 // })
 
-
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
-})
-
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>')
-})
+// app.use('/', (req, res, next) => {
+//   // console.log('In home!');
+//   res.status(300).send('<h1>Hello from Express!</h1>');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
