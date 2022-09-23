c3891710329afb4e2a788186247a4dfca849fa29
kane1997 <648636410@qq.com>
2022-09-23 16:46:17 +1000

    add html files

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const rootDir = require('../utils/path')
 
 // /addproduct =>get
 router.get('/addproduct', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'addproduct.html'))
+  res.sendFile(path.join(rootDir, 'views', 'addproduct.html'))
   //next();   
 });
 
